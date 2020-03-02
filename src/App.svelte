3b3a0c3c3db51f7f48fc77538bbcc50299380391
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-02 14:34:49 +0200

    Adds ContactCard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,44 @@
 <script>
-	export let name;
-	export let age;
+  import ContactCard from "./ContactCard.svelte";
 
-	function incrementAge() {
-		age += 1;
-	}
+  let name = "Max";
+  let age = 30;
+
+  // let uppercaseName; not required!
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Maximilian") {
+    console.log("It runs!");
+    age = 31;
+  }
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Maximilian";
+  }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}, my age is {age}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button on:click={incrementAge}>Increment Age</button>
-</main>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
+
+<ContactCard />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: purple;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world',
-		age: 45
-	}
+	target: document.body
 });
 
 export default app;

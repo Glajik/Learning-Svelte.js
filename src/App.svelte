db24a568e5fa96fdb71909fd475d959c2b9b8081
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-10 23:53:28 +0200

    Now multiple contact cards can be created.

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,33 @@
 <script>
   import ContactCard from "./ContactCard.svelte";
 
-  let name = '';
-  let image = '';
-  let title = '';
+  let userName = '';
+  let userImage = '';
+  let jobTitle = '';
   let description = '';
 
   let formState = 'empty';
 
+  let contacts = [];
+  
   const isEmpty = value => value ? value.trim().length === 0 : true;
 
   const isValid = (...args) => !args.map(isEmpty).includes(true);
 
   const addContact = () => {
-    if (isValid(name, image, title, description)) {
-      formState = 'done';
+    if (isValid(userName, userImage, jobTitle, description)) {
+
+      // Add new contact to contacts
+      contacts = [
+        ...contacts,
+        { userName, jobTitle, userImage, description }
+      ];
+
+      formState = 'empty';
+      userName = '';
+      userImage = '';
+      jobTitle = '';
+      description = '';
       return
     }
     formState = 'invalid';
@@ -25,31 +38,37 @@
 <div id="form">
   <div class="form-control">
     <label for="userName">User Name</label>
-    <input type="text" bind:value={name} id="userName" />
+    <input type="text" bind:value={userName} id="userName" />
   </div>
   <div class="form-control">
     <label for="jobTitle">Job Title</label>
-    <input type="text" bind:value={title} id="jobTitle" />
+    <input type="text" bind:value={jobTitle} id="jobTitle" />
   </div>
   <div class="form-control">
-    <label for="image">Image URL</label>
-    <input type="text" bind:value={image} id="image" />
+    <label for="userImage">Image URL</label>
+    <input type="text" bind:value={userImage} id="userImage" />
   </div>
   <div class="form-control">
     <label for="desc">Description</label>
-    <textarea rows="3" bind:value={description} id="desc" />
+    <textarea rows="3" bind:value={description} id="description" />
   </div>
 </div>
 
 <button on:click={addContact}>Add contact card</button>
 
-{#if formState === 'done'}
-  <ContactCard userName={name} jobTitle={title} {description} userImage={image} />
-{:else if formState === 'invalid'}
+{#if formState === 'invalid'}
   <p>Invalid input.</p>
 {:else}
   <p>Please enter some data and hit the button!</p>
 {/if}
+
+{#each contacts as contact}
+  <ContactCard
+    userName={contact.userName}
+    jobTitle={contact.jobTitle}
+    description={contact.description}
+    userImage={contact.userImage} />
+{/each}
 
 <style>
   #form {

e028f31dfe21a3c7f912e23f64d4b2e0f7980d81
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-13 00:07:25 +0200

    Added styles for MeetupItem component. Wrap meetup output into <section>. Update fake data.

--- meetings-app/src/App.svelte
+++ meetings-app/src/App.svelte
@@ -7,25 +7,33 @@
       id: 'm1',
       title: 'Coding Bootcamp',
       subtitle: 'Learn to code in 2 hours',
-      desctiption: 'In this meetup, we will some experts that teach you',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a',
-      address: '27th Nerd Road, 123123 New Your',
+      description: 'In this meetup, we will have some experts that teach you how to code!',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG',
+      address: '27th Nerd Road, 32523 New York',
       contactEmail: 'code@test.com',
     },
     {
       id: 'm2',
       title: 'Swim Together',
-      subtitle: 'Learn to code in 2 hours',
-      desctiption: 'In this meetup, we will some experts that teach you',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a',
-      address: '27th Nerd Road, 123123 New Your',
-      contactEmail: 'code@test.com',
-    },
-  ]
+      subtitle: 'Let\'s go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com',
+    }
+  ];
 </script>
 
 <Header />
 
-{#each meetups as meetup}
-  <MeetupItem />
-{/each}
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem />
+  {/each}
+</section>
+
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>

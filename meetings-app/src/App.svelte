3c96b607f97c02045c8d720a35368676632ce30a
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-12 23:57:09 +0200

    Added MeetupItem component

--- meetings-app/src/App.svelte
+++ meetings-app/src/App.svelte
@@ -1,5 +1,31 @@
 <script>
-  import Header from './UI/Header.svelte'
+  import Header from './UI/Header.svelte';
+  import MeetupItem from './Meetups/MeetupItem.svelte';
+  
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      desctiption: 'In this meetup, we will some experts that teach you',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a',
+      address: '27th Nerd Road, 123123 New Your',
+      contactEmail: 'code@test.com',
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Learn to code in 2 hours',
+      desctiption: 'In this meetup, we will some experts that teach you',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a',
+      address: '27th Nerd Road, 123123 New Your',
+      contactEmail: 'code@test.com',
+    },
+  ]
 </script>
 
 <Header />
+
+{#each meetups as meetup}
+  <MeetupItem />
+{/each}

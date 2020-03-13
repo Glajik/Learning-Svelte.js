e7ac62b9ce68e2b949c1adbe4ae27e68fa337ddd
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-13 18:11:59 +0200

    Added form to create meetup

--- meetings-app/src/App.svelte
+++ meetings-app/src/App.svelte
@@ -1,8 +1,15 @@
 <script>
   import Header from './UI/Header.svelte';
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
+
+  let title = '';
+  let subtitle = '';
+  let description = '';
+  let imageUrl = '';
+  let address = '';
+  let contactEmail = '';
   
-  const meetups = [
+  let meetups = [
     {
       id: 'm1',
       title: 'Coding Bootcamp',
@@ -22,11 +29,49 @@
       contactEmail: 'swim@test.com',
     }
   ];
+
+  const getId = () => 'm'.concat(Math.floor(Math.random() * 1000000));
+
+  function addMeetup() {
+    const newMeetup = {
+      id: getId(), title, subtitle, description, imageUrl, address, contactEmail,
+    };
+
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
 <Header />
 
 <main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title}>
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle}>
+    </div>
+    <div class="form-control">
+      <label for="description">description</label>
+      <textarea id="description" rows="3"  bind:value={description}/>
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value={address}>
+    </div>
+    <div class="form-control">
+      <label for="imageUrl">Image URL</label>
+      <input type="text" id="imageUrl" bind:value={imageUrl}>
+    </div>
+    <div class="form-control">
+      <label for="email">E-mail</label>
+      <input type="text" id="email" bind:value={contactEmail}>
+    </div>
+    <button type="submit">Create</button>
+  </form>
+
   <MeetupGrid {meetups} />
 </main>
 
@@ -34,4 +79,35 @@
   main {
     margin-top: 4rem;
   }
+
+  input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  border-radius: 3px 3px 0 0;
+  background: white;
+  padding: 0.15rem 0.25rem;
+  transition: border-color 0.1s ease-out;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #e40763;
+  outline: none;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+  width: 100%;
+}
+
+.form-control {
+  padding: 0.5rem 0;
+  width: 100%;
+  margin: 0.25rem 0;
+}
 </style>

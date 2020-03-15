2115e68dcef963ae2d0998a4793d9d1cfe8bd7f3
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-15 18:47:49 +0200

    Added Badge component. Used for favorite badge.

---
+++ course-project-meetings-app/src/UI/Badge.svelte
@@ -0,0 +1,19 @@
+<span>
+  <slot />
+</span>
+
+<style>
+  span {
+    display: inline-block;
+    margin: 0 0.25rem;
+    padding: 0 0.5rem;
+    padding-top: 3px;
+    border-radius: 3px;
+    border: 1px solid #cf0056;
+    background: #cf0056;
+    color: white;
+    font-family: 'Lato', sans-serif;
+    font-size: 0.8rem;
+    text-align: center
+  }
+</style>

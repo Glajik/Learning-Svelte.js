4c2c164e00392032c0d0da984e051056f7be5513
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-16 18:33:19 +0200

    84. Relying on Automatic Number Conversion

--- section-8-form-and-bindings-app/src/App.svelte
+++ section-8-form-and-bindings-app/src/App.svelte
@@ -3,11 +3,16 @@
   import Toggle from './Toggle.svelte';
   let val = 'Max';
   let selectedOption = 1;
+  let summ = 0;
+  let number = 0;
 
   $: console.log(val);
   $: console.log(selectedOption);
+  $: console.log(summ);
+  $: summ += number;
 </script>
 
 <input type="text" bind:value={val}>
 <CustomInput bind:val={val} />
 <Toggle bind:chosenOption={selectedOption}/>
+<input type="number" bind:value={number}>
